e6c648d33fcc8798b42562981cb32c4df53b56d6
LexUx <59761084+Ambmoney@users.noreply.github.com>
2022-05-20 23:30:09 -0700

    Add files via upload
    Touch event practice

---
+++ touchEvents/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("touchstart", e => {
+  console.log("Start")
+})
+
+document.addEventListener("touchmove", e => {
+  console.log("Move")
+})
+
+document.addEventListener("touchend", e => {
+  console.log("End")
+})
